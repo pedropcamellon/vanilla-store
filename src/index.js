b8cb7c849520f9f384979e09a1176453de095ff9
pedropcamellon <pedropcamellon@gmail.com>
2022-11-02 20:07:35 +0000

    Import products.json file. Add reference to HTML elems

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+console.clear();
+
+import prodsjsonfile from "./products.json";
+
 // import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-  <h1>Hello Vanilla!</h1>
-`;
+/**
+ *  HTML Elements
+ */
+// Navbar
+const navbarTotalCartItems = document.querySelector(
+  ".navbar__total-cart-items"
+);
+
+// Header
+const bannerBagBtn = document.querySelector(".banner__cta-btn");
+
+// Cart
+const cartSection = document.querySelector(".cart-section");
+const cartOverlay = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".cart__close-cart-btn");
+const cartBtn = document.querySelector(".navbar__cart-btn");
+const removeCartItem = document.querySelector(".cart-item__remove");
+const cartTotal = document.querySelector(".cart-total");
+const clearCart = document.querySelector(".clear-cart");
+const cartContent = document.querySelector(".cart__content");
+
+// Products Section
+const productsList = document.querySelector(".products__list");
+const product = document.querySelector(".product");
+const productBagBtn = document.querySelector(".product__bag-btn");
